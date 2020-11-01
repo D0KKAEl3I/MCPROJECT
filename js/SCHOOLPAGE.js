aa0916083e08d019737de57a9d21c3c4387ed81c
D0KKAEl3I <dldudtjr850@gmail.com>
2020-11-01 19:28:46 +0900

    제출단계!

--- js/SCHOOLPAGE.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 124){
-
-    document.getElementById('sectionNav').style.top='0'
-    document.getElementById('sectionNav').style.display='flex'
-    console.log(document.getElementById('sectionNav'))
-    }
-}
