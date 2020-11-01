33cdb13748533ddd8f35e4d25a2ebbcb0fee40f6
D0KKAEl3I <dldudtjr850@gmail.com>
2020-11-01 13:57:02 +0900

    사이드내비게이션 추가

--- js/PROJECT.js
+++ js/PROJECT.js
@@ -1,10 +1,8 @@
-var sideNav
-window.onload = function() {
-    sideNav = document.getElementById('sideNav');
-}
-console.log(document.getElementById('sideNav'))
-window.onscroll = function(){
+document.onscroll = function(){
+    var sideNav = document.getElementById('sideNav');
     if(document.documentElement.scrollTop > 200) {
+        
+        
         sideNav.style.top='30%'
         sideNav.style.opacity='1'
         
@@ -14,4 +12,13 @@ window.onscroll = function(){
         sideNav.style.opacity='0'
     }
 }
+
+var navHeight = true
+function morphNav(){
+    console.log(navHeight)
+    if(navHeight){document.getElementById('sideNav').style.height="230px"; navHeight = !navHeight;}    
+    else {document.getElementById('sideNav').style.height="70px"; navHeight = !navHeight;}  
+}
+document.getElementById('sideNav').addEventListener('mouseover', morphNav)
+document.getElementById('sideNav').addEventListener('mouseout', morphNav)
 
