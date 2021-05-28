4678e3248947e1538667def4a5915069699fa106
jisoo03010 <sdh20200305@sdh.hs.kr>
2021-05-28 16:54:11 +0900

    .

--- AngularPublish/src/app/app.module.ts
+++ AngularPublish/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header.component';
 import { SlideComponent } from './components/slide.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideComponent
+    SlideComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
